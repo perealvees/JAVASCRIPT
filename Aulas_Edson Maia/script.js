cda0bdb57b7b8b254eec2e8ad29401ac580705c8
perealvees <perealvees@gmail.com>
2023-08-24 21:28:43 -0300

    template String

--- Aulas_Edson Maia/script.js
+++ Aulas_Edson Maia/script.js
@@ -1,2 +1,18 @@
 let msg1 = 'mensagem aqui'
 let msg2 = 'outra mensagem aqui'
+let nome = 'Priscilla'
+let minhaAltura = 1.80
+let meuPeso = 68
+let minhaIdade = 32
+let ehDoador = true
+
+
+const PI = 3.14
+const TAXA = 0.5
+
+console.log(`Meu nome é ${nome}.`)
+console.log(`Minha idade é ${minhaIdade}.`)
+console.log(`Meu peso é ${meuPeso}.`)
+console.log(`Minha altura é de ${minhaAltura}.`)
+console.log(`O número de PI é ${PI}.`)
+console.log(`Faço esse trabalho como uma ${TAXA}.`)
