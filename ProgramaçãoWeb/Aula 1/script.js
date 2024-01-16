cfa28aabafbabeb1f7736a4d27620622cbaea4f1
Priscilla Alves <perealvees@gmail.com>
2024-01-16 19:11:28 -0300

    aula 3

---
+++ ProgramaçãoWeb/Aula 1/script.js
@@ -0,0 +1,3 @@
+document.getElementById("texto").innerHTML="Meu primeiro texto em <b>JS<b>!"
+alert("oi, isso também é uma saída de texto!");
+console.log("Ou se preferir, usando o console");
---
+++ ProgramaçãoWeb/Aula 2/script.js
@@ -0,0 +1,4 @@
+document.getElementById("texto").innerHTML="Meu primeiro texto em <b>JS<b>!"
+alert("oi, isso também é uma saída de texto!");
+console.log("Ou se preferir, usando o console");
+console.log(5+5);
