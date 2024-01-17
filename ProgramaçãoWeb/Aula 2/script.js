01321efa51cd71fe336951ac9db4de193391eaff
Priscilla Alves <perealvees@gmail.com>
2024-01-17 11:10:28 -0300

    aula 6

--- ProgramaçãoWeb/Aula 2/script.js
+++ ProgramaçãoWeb/Aula 2/script.js
@@ -1,4 +1,12 @@
-document.getElementById("texto").innerHTML="Meu primeiro texto em <b>JS<b>!"
+/*document.getElementById("texto").innerHTML="Meu primeiro texto em <b>JS<b>!"
 alert("oi, isso também é uma saída de texto!");
 console.log("Ou se preferir, usando o console");
-console.log(5+5);
+console.log(5+5); */
+
+a = 5; b = 8; c = a+b;
+
+
+console.log(c+c);
+console.log(c*a*b);
+document.write(c);
+document.getElementById("texto").innerHTML="e agora?"
---
+++ ProgramaçãoWeb/Aula 6/script.js
@@ -0,0 +1,11 @@
+//var a = 30; var b = 50; var c = a + b;
+//document.write(c);//
+
+//também podemos declarar assim:
+
+//DECLARA O NOME DAS VARIÁVEIS
+var d, e, f;
+//DEPOIS A ATRIBUIÇÃO DOS VALORES
+d = 5, e= 2, f= 8*10;
+
+document.write(f+d);
