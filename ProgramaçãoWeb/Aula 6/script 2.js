ffb0d00366c356d4fe78f24a7b6c17281bf87cb9
Priscilla Alves <perealvees@gmail.com>
2024-01-17 14:16:44 -0300

    aula 7

---
+++ ProgramaçãoWeb/Aula 6/script 2.js
@@ -0,0 +1,12 @@
+var nome, sobrenome, nomeCompleto, idade, soma;
+
+nome = "João";
+sobrenome = "da Silva";
+idade = 50;
+nomeCompleto = nome + sobrenome;
+
+pessoa = nome + " " + idade + " " + "anos";
+soma = idade + 10;
+
+document.getElementById("texto").innerHTML = pessoa;
+//document.getElementById("texto").innerHTML = soma;
---
+++ ProgramaçãoWeb/Aula 6/script 7.js
@@ -0,0 +1,23 @@
+/* Operadores: 
+
+São 6 categorias:
+
+1 - Aritmetícos
+2 - Atribuição
+3 - Sequencia
+4 - Comparação
+5 - Condicional
+6 - Lógicos
+
+Aritméticos */
+var n1, n2, n3;
+n1 = 1; n2 = 9; n3 = 20;
+
+result = n2 + n3;
+
+document.getElementById("texto").innerHTML=result;
+
+
+
+
+
