20ff4c1b0feca3f7c0180193ee7a7db5d8219b14
Priscilla Alves <perealvees@gmail.com>
2024-01-17 20:38:15 -0300

    operadores

--- ProgramaçãoWeb/Aula 6/script 7.js
+++ ProgramaçãoWeb/Aula 6/script 7.js
@@ -2,20 +2,54 @@
 
 São 6 categorias:
 
-
-2 - Atribuição
-3 - Sequencia
-4 - Comparação
 5 - Condicional
 6 - Lógicos
 
 
-1 - Aritmetícos
+1 - Aritmetícos 
+
 var n1, n2, n3;
 n1 = 1; n2 = 9; n3 = 20;
 
-result = n2 + n3;
+result = ++n2 + ++n3;
 
-document.getElementById("texto").innerHTML=result;
+document.getElementById("texto").innerHTML=result; 
 
-*/
+2 - Atribuição 
+
+var n1, n2, total;
+n1 = 5; n2 = 3, total;
+
+n1 += n1+n2;
+document.getElementById("texto").innerHTML="n1";
+
+3 - Sequencia 
+var nome, sobrenome;
+nome = "Priscilla ";
+sobrenome = "Alves";
+
+document.getElementById("texto").innerHTML=nome + sobrenome;
+
+
+4 - Comparação 
+Verdadeiro ou falso 
+
+== igual: 8 = 8
+=== se o valor e tipo é igual
+!= diferente
+!== diferente e tipo diferente
+< menor
+> maior
+
+
+var n1, n2, total;
+n1 = 50; n2 = 50; 
+
+total = (n2 == n1);
+alert(total) */
+
+var n1, n2, total;
+n1 = 50; n2 = 50; 
+
+total(n1 === n2);
+alert(total);
