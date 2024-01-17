5804cb1310abc0512cf59405f98cf3d7cf2b501c
Priscilla Alves <perealvees@gmail.com>
2024-01-17 14:20:25 -0300

    Update script 7.js

--- ProgramaçãoWeb/Aula 6/script 7.js
+++ ProgramaçãoWeb/Aula 6/script 7.js
@@ -2,14 +2,15 @@
 
 São 6 categorias:
 
-1 - Aritmetícos
+
 2 - Atribuição
 3 - Sequencia
 4 - Comparação
 5 - Condicional
 6 - Lógicos
 
-Aritméticos */
+
+1 - Aritmetícos
 var n1, n2, n3;
 n1 = 1; n2 = 9; n3 = 20;
 
@@ -17,7 +18,4 @@ result = n2 + n3;
 
 document.getElementById("texto").innerHTML=result;
 
-
-
-
-
+*/
