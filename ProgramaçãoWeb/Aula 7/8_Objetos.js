07a23a8f23c6a0b6062f68173441b44c254b2343
Priscilla Alves <perealvees@gmail.com>
2024-01-19 18:57:58 -0300

    object

---
+++ ProgramaçãoWeb/Aula 7/8_Objetos.js
@@ -0,0 +1,6 @@
+/*objetos são variaveis com muitos valores dentro
+ ex: const caroo = {marca: "ford", modelo: "ka", ano: 2018}   
+ Os valores dentro de um objeto são chamados de PROPRIEDADES
+ Objetos também odem ter métodos. Ou seja, uma função dentro de uma propriedade
+ 
+*/ 
