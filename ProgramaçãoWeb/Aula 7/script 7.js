a2a625af318c94d46e3e8714cd81a60bc04d16ed
Priscilla Alves <perealvees@gmail.com>
2024-01-19 18:46:51 -0300

    funções

---
+++ ProgramaçãoWeb/Aula 7/script 7.js
@@ -0,0 +1,55 @@
+/* Operadores: 
+
+São 6 categorias:
+
+5 - Condicional
+6 - Lógicos
+
+
+1 - Aritmetícos 
+
+var n1, n2, n3;
+n1 = 1; n2 = 9; n3 = 20;
+
+result = ++n2 + ++n3;
+
+document.getElementById("texto").innerHTML=result; 
+
+2 - Atribuição 
+
+var n1, n2, total;
+n1 = 5; n2 = 3, total;
+
+n1 += n1+n2;
+document.getElementById("texto").innerHTML="n1";
+
+3 - Sequencia 
+var nome, sobrenome;
+nome = "Priscilla ";
+sobrenome = "Alves";
+
+document.getElementById("texto").innerHTML=nome + sobrenome;
+
+
+4 - Comparação 
+Verdadeiro ou falso 
+
+== igual: 8 = 8
+=== se o valor e tipo é igual
+!= diferente
+!== diferente e tipo diferente
+< menor
+> maior
+
+
+var n1, n2, total;
+n1 = 50; n2 = 50; 
+
+total = (n2 == n1);
+alert(total) */
+
+var n1, n2, total;
+n1 = 50; n2 = 50; 
+
+total(n1 === n2);
+alert(total);
---
+++ ProgramaçãoWeb/Aula 7/script 8.js
@@ -0,0 +1,22 @@
+/*function media (v1, v2, v3, v4) {
+    return (v1+v2+v3+v4)/4;
+}
+
+var media = (10,10,10,10);
+
+alert(media)*/
+
+/*var vr = 9;
+var vd = 5.50;
+
+function dolar (vr, vd) {
+    return vr * vd;
+}
+
+var total = dolar (10, 5.50);
+alert("O valor em reais é de R$ "+ vr +" e o valor em dolar é "+ total );*/
+
+/*function alertHello(){
+    return alert('e aí, pessoal!')
+}*/
+
