dda63717a54f228ad0c159128b89a2a77cd2b51f
Priscilla Alves <perealvees@gmail.com>
2024-02-14 20:44:08 -0300

    ex011

---
+++ Mini Exercícios/Aula 11/exx008.js
@@ -0,0 +1,19 @@
+/* var pais = 'oiajsdi'
+
+    console.log(`Vivendo em ${pais}`)
+    if(pais == "Brasil") {
+        console.log("Você é Brasileiro!")
+    } else {
+        console.log("Você é estrangeiro!")
+    } */
+    
+    var vel = 61
+    
+    console.log(`Você passou a ${vel}KM.`)
+    if(vel > 60){
+        console.log('Você ultrapassou o limite permitido. Multado!')
+    } else {
+        console.log('Dirija sempre com cinto de segurança!')
+    }
+
+    
