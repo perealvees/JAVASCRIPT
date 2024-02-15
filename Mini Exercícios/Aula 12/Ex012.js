f5cb1f2144421074a5b275ebe61444412e66cf37
Priscilla Alves <perealvees@gmail.com>
2024-02-15 20:27:56 -0300

    while / do while

---
+++ Mini Exercícios/Aula 12/Ex012.js
@@ -0,0 +1,11 @@
+/* var fatia = 1
+
+while (fatia <= 10) {
+        console.log(`Você comeu a fatia ${fatia} `)
+    fatia ++
+} */
+
+var fatia = 1
+do {
+    console.log(`Você comeu a fatia ${fatia}`)
+} while (fatia <= 40)
