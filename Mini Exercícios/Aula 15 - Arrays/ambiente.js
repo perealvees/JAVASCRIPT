e00bf8a97bf01f0ecebbfd24f7cfce148d305b62
Priscilla Alves <perealvees@gmail.com>
2024-02-16 15:09:39 -0300

    Create ambiente.js

---
+++ Mini Exercícios/Aula 15 - Arrays/ambiente.js
@@ -0,0 +1,10 @@
+let num = [5,8,4,7,1]
+
+//num.push(6)   -   usando o num.push, ele adiciona uma variável  no ultimo ínidice
+//num[4] = 4    -   usando o num[] você colocar dentro das chaves a posição que ele deve preencher
+//num.sort()    -   deixar em ordem crescente
+
+// tomar cuidado com as ordens do num.
+
+console.log(`Esse vetor tem no total ${num.length} posições`)
+console.log(`O vetor de posição 2, é o ${num[1]}`)
