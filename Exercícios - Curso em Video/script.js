36cc963b779bad3d098002068b0950da0c79a593
Priscilla Alves <perealvees@gmail.com>
2024-02-19 11:31:26 -0300

    ajuste background

--- Exercícios - Curso em Video/script.js
+++ Exercícios - Curso em Video/script.js
@@ -6,6 +6,7 @@
         var hora = data.getHours()
     - para chamar a mensagem, usar a id da mensagem em seguida do inner.html
     - img.src = 'img/ + nome da foto
+    - PARA ATRIBUIR UMA COR DE ACORDO COM A HORA, USAR O  document.body.style.background =
     
 */
 
@@ -15,20 +16,23 @@ function carregar () {
 var msgm = document.getElementById('msgm')
 var img = document.getElementById('imagem')
 var data = new Date();
-//var hora = data.getHours()
-var hora = 21
+var hora = data.getHours()
+
+
 
 
 msgm.innerHTML=`Agora são ${hora}h`
 
 if(hora >= 0 && hora < 12){
     img.src = 'img/manhã-fotor-20240216185211.png'
+    document.body.style.background = "#FFFF50"
     
 } else if (hora >= 12 && hora <= 18) {
-    console.log('Boa tarde')
     img.src = 'img/tarde-fotor-20240216185248.png'
+    document.body.style.background = "#FF8C00"
 } else {
     img.src = 'img/noite-fotor-20240216185038.png'
+    document.body.style.background = "#2F4F4A"
 }
 
 
